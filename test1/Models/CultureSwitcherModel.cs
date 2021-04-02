7481ab7cc4f8ccd9d1b103339a0ca702fdae9891
alestroc <67910013+alestroc@users.noreply.github.com>
2021-04-02 23:47:30 +0200

    Aggiungere i file di progetto.

---
+++ test1/Models/CultureSwitcherModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace test1.Models
+{
+    public class CultureSwitcherModel
+    {
+        public CultureInfo CurrentUICulture { get; set; }
+        public List<CultureInfo> SupportedCultures { get; set; }
+    }
+}
